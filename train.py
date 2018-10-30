0dae3d4594bce0d3e59a5dbbd90c78b82a9af357
Jay Prombo <prombo@google.com>
2018-10-29 22:29:52 -0400

    added model from nvidia paper

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open('../collected_data/arbitrary/driving_log.csv') as f:
+with open('./collected_data/center_full_lap_counterclockwise/driving_log.csv') as f:
     reader = csv.reader(f)
     for line in reader:
         lines.append(line)
@@ -13,7 +13,7 @@ measurements = []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    impath = '../collected_data/arbitrary/IMG/' + filename
+    impath = './collected_data/center_full_lap_counterclockwise/IMG/' + filename
     image = cv2.imread(impath)
     images.append(image)
     measurement = float(line[3])
@@ -23,13 +23,23 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 model.add(Dense(1))
 
-model.combile(loss='mse', optimizer='adam')
+model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
 
 model.save('model.h5')
