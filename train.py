744280ebd69528283c0b50506620bba2b68111e7
biohazardpb4 <prombus@gmail.com>
2018-11-08 04:07:02 +0000

    moving dropout layer up a few so that lower level features are dropped out instead of the final steering decision

--- train.py
+++ train.py
@@ -59,12 +59,12 @@ model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
-model.add(Dense(10))
 model.add(Dropout(0.2))
+model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer=Adam(lr=0.005))
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=15)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
 
 model.save('model.h5')
 
