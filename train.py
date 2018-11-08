135c0876cc6906fd19f6134c7694d971a4dc26b3
biohazardpb4 <prombus@gmail.com>
2018-11-08 04:17:11 +0000

    adding new model without dropout

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense, Dropout
+from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense
 from keras.optimizers import Adam
 
 model = Sequential()
@@ -59,7 +59,6 @@ model.add(Convolution2D(64, 3, 3, activation='relu'))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
-model.add(Dropout(0.2))
 model.add(Dense(10))
 model.add(Dense(1))
 
