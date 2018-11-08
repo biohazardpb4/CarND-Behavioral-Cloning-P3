d746540ac3c3667ad013534e5442a412a932343f
biohazardpb4 <prombus@gmail.com>
2018-11-08 03:46:15 +0000

    increased learning rate based on advice from dropout blog

--- train.py
+++ train.py
@@ -46,6 +46,7 @@ y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
 from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense, Dropout
+from keras.optimizers import Adam
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
@@ -63,8 +64,8 @@ model.add(Dense(10))
 model.add(Dropout(0.2))
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.compile(loss='mse', optimizer=Adam(lr=0.005))
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=15)
 
 model.save('model.h5')
 
